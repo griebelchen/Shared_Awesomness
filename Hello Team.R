38b0c7ddd0b9fee4e291baf2714fbc5ffdfe98b0
Courtney Campany <courtneycampany@gmail.com>
2017-05-24 16:58:07 +1000

    added introduction script and folder for ann

---
+++ Hello Team.R
@@ -0,0 +1,4 @@
+###This is our repo to share how to do cool stuff in R
+###If you are invited here we think you are awesome...or...you have coding knowledge we plan to leach from you
+
+###lets make folders named for certain cool R things and stick examples in them
