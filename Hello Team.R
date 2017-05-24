9ce85b737cda8ff48ad0b3fc34c945687d340433
griebelchen <griebel.anne@gmail.com>
2017-05-24 17:05:43 +1000

    Update Hello Team.R

--- Hello Team.R
+++ Hello Team.R
@@ -2,3 +2,5 @@
 ###If you are invited here we think you are awesome...or...you have coding knowledge we plan to leach from you
 
 ###lets make folders named for certain cool R things and stick examples in them
+
+###yeah, that'd be dope
